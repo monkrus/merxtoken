a8218a207409e57ee42f6de7d19874c241641d90
Sergei Stadnik <sergeisqa@gmail.com>
2019-04-23 08:17:21 -0700

    Back end succesfully  tested

--- contracts/MerxToken.sol
+++ contracts/MerxToken.sol
@@ -49,7 +49,7 @@ contract MerxToken {
 
         return true;
     }
-
+   //allows transfer
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
